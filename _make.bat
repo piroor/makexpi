b780bce313f6e7fadd7fd57dc945cc9baf29b49b
piro <piro@599a83e7-65a4-db11-8015-0010dcdd6dc2>
2007-02-26 17:07:29 +0000

    ビルド用バッチファイル更新
    git-svn-id: https://www.cozmixng.org/repos/piro/make-xpi/trunk@179 599a83e7-65a4-db11-8015-0010dcdd6dc2

--- _make.bat
+++ _make.bat
@@ -1,20 +1,16 @@
 setlocal
 set appname=%~n0
 
-
 d:
 cd d:\data\codes\%appname%\trunk
 
-for /F "tokens=1-3 delims=/ " %%a in ('date /t') do set DATES=%%a%%b%%c
-
-del "o:\xul\xpi\%appname%.xpi"
-del "o:\xul\xpi\%appname%_en.xpi"
-del "o:\xul\xpi\%appname%.lzh"
-
-chmod -cfr 644 *.jar *.js *.light *.inf *.rdf *.cfg *.manifest
+rmdir "temp" /s /q
+del "%appname%.xpi"
+del "%appname%_en.xpi"
+del "%appname%.lzh"
 
 
-
+:CREATETEMPFILES
 mkdir temp
 xcopy content temp\content /i /s
 xcopy locale temp\locale /i /s
@@ -23,58 +19,80 @@ xcopy defaults temp\defaults /i /s
 xcopy *.js temp\ /i
 xcopy *.rdf temp\ /i
 xcopy *.manifest temp\ /i
+
+xcopy *.cfg temp\ /i
+xcopy *.light temp\ /i
 cd temp
 
+chmod -cfr 644 *.jar *.js *.light *.inf *.rdf *.cfg *.manifest
 
 
+:MAKEJAR
 mkdir "chrome"
 zip -r0 "chrome\%appname%.jar" content locale skin
 
-del locale.inf
-copy o:\xul\codes\make-xpi\ja.inf .\locale.inf
-del options.inf
-copy "o:\xul\codes\make-xpi\options.%appname%.ja.inf" .\options.inf
-zip -9 o:\xul\xpi\%appname%.xpi *.js *.light *.inf *.rdf *.cfg *.manifest
-zip -9 -r o:\xul\xpi\%appname%.xpi chrome
-zip -9 -r o:\xul\xpi\%appname%.xpi defaults
+
+IF EXIST ..\install.js GOTO MAKEOLD
+GOTO MAKENEW
+
+:MAKEOLD
+copy ..\ja.inf .\locale.inf
+copy "..\options.%appname%.ja.inf" .\options.inf
+chmod -cf 644 *.inf
+
+:MAKENEW
+zip -9 "..\%appname%.xpi" *.js *.light *.inf *.rdf *.cfg *.manifest
+zip -9 -r "..\%appname%.xpi" chrome
+zip -9 -r "..\%appname%.xpi" defaults
 
 
-IF EXIST readme.txt GOTO MAKELZH
-GOTO MAKEEN
+
+IF EXIST ..\readme.txt GOTO MAKELZH
+IF EXIST ..\install.js GOTO MAKEENOLD
+GOTO DELETETEMPFILES
 
 :MAKELZH
-c:\apps\Dos\unlha\unlha.exe a -m0 o:\xul\xpi\%appname%.lzh o:\xul\xpi\%appname%.xpi readme.txt
+c:\apps\Dos\unlha\unlha.exe a -m0 ..\%appname%.lzh ..\%appname%.xpi ..\readme.txt
 
 
-:MAKEEN
-del locale.inf
-copy o:\xul\codes\make-xpi\en.inf .\locale.inf
-del options.inf
-copy "o:\xul\codes\make-xpi\options.%appname%.en.inf" .\options.inf
+
+:MAKEENOLD
+copy ..\en.inf .\locale.inf
+copy "..\options.%appname%.en.inf" .\options.inf
 chmod -cf 644 *.inf
-zip -9 o:\xul\xpi\%appname%_en.xpi *.js *.light *.inf *.rdf *.cfg *.manifest
-zip -9 -r o:\xul\xpi\%appname%_en.xpi chrome
-zip -9 -r o:\xul\xpi\%appname%_en.xpi defaults
+zip -9 "..\%appname%_en.xpi" *.js *.light *.inf *.rdf *.cfg *.manifest
+zip -9 -r "..\%appname%_en.xpi" chrome
+zip -9 -r "..\%appname%_en.xpi" defaults
 
 
 
-IF EXIST readme.txt GOTO COPYLZH
-copy o:\xul\xpi\%appname%.xpi c:\apps\win\other\mozilla\_packages\%appname%_%DATES%.xpi.zip
-GOTO COPYFILES
-
-:COPYLZH
-copy o:\xul\xpi\%appname%.lzh c:\apps\win\other\mozilla\_packages\%appname%_%DATES%.lzh
-
-
-
-:COPYFILES
+:DELETETEMPFILES
 
 cd ..
 rmdir "temp" /s /q
 
 
-rem copy %appname%.jar C:\Apps\Win\Other\Mozilla\bin\chrome\%appname%.jar
-rem copy %appname%.jar C:\Apps\Win\Other\Mozilla\bin16\chrome\%appname%.jar
-rem copy %appname%.jar "C:\Apps\Win\Other\Netscape\Netscape 7\chrome\%appname%.jar"
+
+
+
+del "o:\xul\xpi\%appname%.xpi"
+del "o:\xul\xpi\%appname%_en.xpi"
+del "o:\xul\xpi\%appname%.lzh"
+
+for /F "tokens=1-3 delims=/ " %%a in ('date /t') do set DATES=%%a%%b%%c
+
+IF EXIST readme.txt GOTO COPYLZH
+copy %appname%.xpi c:\apps\win\other\mozilla\_packages\%appname%_%DATES%.xpi.zip
+GOTO MOVEFILES
+
+:COPYLZH
+copy %appname%.lzh c:\apps\win\other\mozilla\_packages\%appname%_%DATES%.lzh
+
+:MOVEFILES
+mv %appname%.xpi o:\xul\xpi\
+mv %appname%_en.xpi o:\xul\xpi\
+mv %appname%.lzh o:\xul\xpi\
+
+
 
 endlocal
