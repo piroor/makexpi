3960659efd0c2ba3a83e2614762e4683f5eb4e38
piro <piro@599a83e7-65a4-db11-8015-0010dcdd6dc2>
2007-04-09 06:57:29 +0000

    署名済みパッケージの作成に関する記述を追加
    git-svn-id: https://www.cozmixng.org/repos/piro/make-xpi/trunk@326 599a83e7-65a4-db11-8015-0010dcdd6dc2

--- make_new.bat
+++ make_new.bat
@@ -1,34 +1,49 @@
 setlocal
 set appname=%~n0
 
-rmdir "temp" /s /q
+rmdir "jar_temp" /s /q
+rmdir "xpi_temp" /s /q
 del "%appname%.xpi"
 del "%appname%_en.xpi"
 del "%appname%.lzh"
 
 
-:CREATETEMPFILES
-mkdir temp
-xcopy content temp\content /i /s
-xcopy locale temp\locale /i /s
-xcopy skin temp\skin /i /s
-xcopy defaults temp\defaults /i /s
-xcopy components temp\components /i /s
-xcopy chrome temp\chrome /i /s
-xcopy *.js temp\ /i
-xcopy *.rdf temp\ /i
-xcopy *.manifest temp\ /i
+:CREATEJARTEMPFILES
+mkdir jar_temp
+xcopy content jar_temp\content /i /s
+xcopy locale jar_temp\locale /i /s
+xcopy skin jar_temp\skin /i /s
 
-xcopy *.cfg temp\ /i
-xcopy *.light temp\ /i
-cd temp
+cd jar_temp
+chmod -cfr 644 *.jar *.js *.light *.inf *.rdf *.cfg *.manifest
+
+:MAKEJAR
+rem cd ..
+rem signtool -d "証明書データベースのパス" -k "証明書の名前" -p "パスワード" -Z "chrome\%appname%.jar" jar_temp
+zip -r0 "%appname%.jar" content locale skin
+cd ..
+
+
+:CREATEXPITEMPFILES
+mkdir xpi_temp
+xcopy defaults xpi_temp\defaults /i /s
+xcopy components xpi_temp\components /i /s
+xcopy chrome xpi_temp\chrome /i /s
+xcopy *.js xpi_temp\ /i
+xcopy *.rdf xpi_temp\ /i
+xcopy *.manifest xpi_temp\ /i
+xcopy *.cfg xpi_temp\ /i
+xcopy *.light xpi_temp\ /i
+
+cd xpi_temp
+
+mkdir chrome
+xcopy ..\jar_temp\*.jar chrome\ /i /s
 
 chmod -cfr 644 *.jar *.js *.light *.inf *.rdf *.cfg *.manifest
 
+:MAKEXPI
 
-:MAKEJAR
-mkdir "chrome"
-zip -r0 "chrome\%appname%.jar" content locale skin
 
 
 IF EXIST ..\install.js GOTO MAKEOLD
@@ -40,6 +55,9 @@ copy "..\options.%appname%.ja.inf" .\options.inf
 chmod -cf 644 *.inf
 
 :MAKENEW
+rem cd ..
+rem signtool -d "証明書データベースのパス" -k "証明書の名前" -p "パスワード" -X -Z "..\%appname%.xpi" xpi_temp
+rem cd xpi_temp
 zip -9 "..\%appname%.xpi" *.js *.light *.inf *.rdf *.cfg *.manifest
 zip -9 -r "..\%appname%.xpi" chrome defaults components
 
@@ -58,6 +76,9 @@ unlha.exe a -m0 ..\%appname%.lzh ..\%appname%.xpi ..\readme.txt
 copy ..\en.inf .\locale.inf
 copy "..\options.%appname%.en.inf" .\options.inf
 chmod -cf 644 *.inf
+rem cd ..
+rem signtool -d "証明書データベースのパス" -k "証明書の名前" -p "パスワード" -X -Z "..\%appname%_en.xpi" xpi_temp
+rem cd xpi_temp
 zip -9 "..\%appname%_en.xpi" *.js *.light *.inf *.rdf *.cfg *.manifest
 zip -9 -r "..\%appname%_en.xpi" chrome defaults components
 
@@ -66,6 +87,22 @@ zip -9 -r "..\%appname%_en.xpi" chrome defaults components
 :DELETETEMPFILES
 
 cd ..
-rmdir "temp" /s /q
+rmdir "jar_temp" /s /q
+rmdir "xpi_temp" /s /q
+
+
+IF EXIST meta GOTO COPYFORMETAPACKAGE
+GOTO ENDBATCH
+
+
+
+:COPYFORMETAPACKAGE
+
+del "meta\%appname%.xpi"
+copy "%appname%.xpi" meta\
+
+
+
+:ENDBATCH
 
 endlocal
