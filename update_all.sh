03c7afe8c17ab6891e5662ecb9c914fae91ba7bc
SHIMODA Hiroshi <shimoda@clear-code.com>
2010-11-09 11:39:46 +0900

    a script to update all of local repositories (and working copies)

---
+++ update_all.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+for dirname in *
+do
+
+	if [ -d $dirname/.svn ]
+	then
+		cd $dirname
+		echo "cleanup: $dirname"
+		svn cleanup
+		echo "update: $dirname"
+		svn up
+		cd ..
+	fi
+
+	if [ -d $dirname/.git ]
+	then
+		cd $dirname
+		echo "pull: $dirname"
+		git pull
+		echo "submodule update: $dirname"
+		git submodule update --init ""
+		cd ..
+	fi
+
+done
+
