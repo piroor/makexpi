f20fddd275655998067c782a67d1e899940c14da
Piro / YUKI Hiroshi <piro.outsider.reflex@gmail.com>
2013-03-30 03:55:59 +0900

    Remove needless comment

--- make_release_xpi.sh
+++ make_release_xpi.sh
@@ -51,7 +51,6 @@ echo "$version" > release_version.txt
 cp install.rdf install.rdf.bak
 
 # リリースビルド用として、install.rdfを書き換える。
-# バージョン番号の末尾に今日の日付を付ける。
 $sed -e "s/(em:version=\")[^\"]*/\\1$version/" \
      -i install.rdf
 update_rdf="${package_name}.update.rdf"
